b7e9a16dd87254e9780dd093b14264420f72c6b2
Oliver Boom <oliver.boom@hotmail.com>
2018-11-05 16:43:34 +0000

    Refactoring and adding of ODE solver, plots look incorrect atm

--- eroscode.py
+++ eroscode.py
@@ -1,85 +1,115 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
-C_D = 1       # Drag coefficient
-C_H = 0.1     # Heat transfer coefficient
-Q = 1E7       # Heat of ablation
-C_L = 1E-3    # Lift coefficient
-alpha = 0.3   # Dispersion coefficient
-H = 8000      # Atmoshperic scale height
-rho_0 = 1.2   # Atmoshpheric density at sea level
-R_E = 6.371e6 # Radius of Earth
-r = 3         # Radius of asteroid
-g_E = 9.81    # grav constant on Earth
-    
-def initial():
-    x_init = 0
-    r_init = 3
-    v_init = 20e3
-    rho_m_init = 3300
-    V_init = Volume(r)
+def inital_parameter():
+    global C_D
+    global C_H
+    global Q
+    global C_L
+    global alpha
+    global H
+    global rho_0
+    global R_E
+    global r
+    global g_E
+    C_D = 1  # Drag coefficient
+    C_H = 0.1  # Heat transfer coefficient
+    Q = 1E7  # Heat of ablation
+    C_L = 1E-3  # Lift coefficient
+    alpha = 0.3  # Dispersion coefficient
+    H = 8000  # Atmospheric scale height
+    rho_0 = 1.2  # Atmospheric density at sea level
+    R_E = 6.371e6  # Radius of Earth
+    r = 3  # Radius of asteroid
+    g_E = 9.81  # gravity constant on Earth
+
+
+def initial_variables():
+    v_init = 20e3  # initial body axes velocity of asteroid
+    m_init = 1000 # initial mass in kg
+    theta_init = 40 * np.pi / 180
+    z_init = 100e3 # initial altitude in metres
+    x_init = 0  # initial horizontal displacement
+    r_init = 3  # initial radius of the asteroid
+
+    '''
+    Where to include data around initial volume and mass
+    V_init = Volume(r)  # initial volume
     m_init = V_init * rho_m_init
-    theta_init = 18.3 * np.pi /180
-    z_init = 100e3
-    
-    return np.array([v_init, m_init, theta_init, 
-                     z_init, x_init, r_init, rho_m_init])
+    rho_m_init = 3300  # initial asteroid density
+    '''
 
-def KE(m,v):
-    return 0.5 * m * v**2
+    return np.array([v_init, m_init,
+                     theta_init, z_init, x_init, r_init])
 
-def dv(z, r, theta):
-    return ((-C_D * rho_a(z) * A(r) * v**3)/(2*m) + g * np.sin(theta)) 
-    
+
+def KE(m, v):
+    return 0.5 * m * v ** 2
+
+
+def dv(z, r, v, theta, m):
+    return ((-C_D * rho_a(z) * A(r) * v ** 3) / (2 * m) + g_E * np.sin(theta))
+
 
 def rho_a(z):
-    return rho_0 * np.exp(-z/H)
+    return rho_0 * np.exp(-z / H)
 
-def dz(theta,v):
-    return -v*np.sin(theta)
+
+def dz(theta, v):
+    return -v * np.sin(theta)
+
 
 def dtheta(theta, v, z, m, r):
-    return ((g_E*np.cos(theta)/v) - ((C_L*rho_a(z)*A(r)*v)/(2*m)) - ((v*np.cos(theta))/(R_E + z)))
-    
-    
-def dx(theta, v, z):
-    return (v*np.cos(theta))/(1 + z/R_E)
+    return ((g_E * np.cos(theta) / v) - ((C_L * rho_a(z) * A(r) * v) / (2 * m)) - ((v * np.cos(theta)) / (R_E + z)))
 
-def dm(z,v):
-    return (-C_H * rho_a(z) * A(r) * v**3)/(2*Q)
+
+def dx(theta, v, z):
+    return (v * np.cos(theta)) / (1 + z / R_E)
+
+
+def dm(z, v):
+    return (-C_H * rho_a(z) * A(r) * v ** 3) / (2 * Q)
+
 
 def A(r):
-    return np.pi * r**2
+    return np.pi * r ** 2
+
 
 def Volume(r):
-    return 4/3 * np.pi * r**3
+    return 4 / 3 * np.pi * r ** 3
 
-def dr():
-    return (7/2*alpha*rho_a(z)/rho_m )   
+
+def dr(z, m, r):
+    return (7 / 2 * alpha * rho_a(z) / (m/Volume(r))) # need to decide whether to store volume
+
 
 def f(t, state):
-    
     f = np.zeros_like(state)
-    v, m, theta, z, x, r, rho_m = state
-    f[0] = dv(z,r)
-    f[1] = dm(z,v)
-    f[2] = dtheta(theta,v,z,m,r)
-    
-state0 = initial()
+    v, m, theta, z, x, r = state
+    f[0] = dv(z, r, v, theta, m)
+    f[1] = dm(z, v)
+    f[2] = dtheta(theta, v, z, m, r)
+    f[3] = dz(theta, v)
+    f[4] = dx(theta, v, z)
+    f[5] = dr(z, m, r)
+    return f
 
-dt = 0.1
-t = np.arange(0.0, 40.0, dt)
 
-    
 def main():
-    initial()
-    max_iter = 100
-    
-    
-    
-#    while n < max_iter:
-        
+    inital_parameter()
+    state0 = initial_variables()
+    dt = 0.1
+    t = np.arange(0.0, 40.0, dt)
+    states = odeint(f, state0, t, tfirst=True)
+    vs = states[:, 0]
+    dz = states[:, 3]
+    plt.plot(t, vs)
+    plt.show()
+    plt.plot(t, dz)
+    plt.show()
+
 # =============================================================================
 # def f(t, y):
 #     f = np.zeros_like(y)
@@ -91,13 +121,7 @@ def main():
 #     f[2] = x * y - beta * z
 #     
 # =============================================================================
-    
-    
-    
-    
 
-    
-    
-
-#if __name__ == "main":
-#    main()
+main()
+if __name__ == "main":
+    main()
---
+++ test_compulory.py
@@ -0,0 +1,12 @@
+'''
+Whether the scenario is an airburst or a cratering event
+- Give extremely strong astroid material and extremely weak
+material and see if the airburst/crater boolean comes back
+as true or false
+- include boundary cases for all input
+The peak kinetic energy loss per km (airburst)
+The burst altitude (airburst)
+The total kinetic energy loss at burst (airburst)
+The mass and speed of the remnants of the asteroid that strike(s) the ground (airburst and cratering)
+'''
+
