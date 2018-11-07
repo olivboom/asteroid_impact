0aa579822394089026611e0247250b19f3183732
Hamed Khandahari <HK2715@ic.ac.uk>
2018-11-07 13:35:05 +0000

    Neater equations, but still out by a huuuuge factor (unresolved)

--- plotting_analytical.py
+++ plotting_analytical.py
@@ -49,57 +49,68 @@ def initial_parameter():
     global rho_0
     global z_init
     global v_init
+    global z_init_m
+
 
     print("here")
 
-    C_D = 1
-    m = 1e4
-    A = 10
-    Theta = 30 * (np.pi)/180
+    C_D = 1    
+    m = 12e3
+    A = np.pi * (19.5/2)**2
+    Theta = 20 * (np.pi)/180
     H = 8000
     rho_0 = 1.2
-    z_init = 1e5
-    v_init = 2000
+    z_init = 100e3
+    v_init = 19e3
 
 
 
 
 def plot():
-    """ Plots analytical solution for simple case specified in project notes"""
     initial_parameter()
-    num = 2000000
-    z = np.linspace(0, z_init, num)
-    step = (z_init/num )/1000
-    print(step)
-
-    const = -(rho_0 * A * H)/(2*m*np.sin(Theta))
-    ln_v = const * np.exp(-z/H) - const* np.exp(-z_init/H) + np.log(v_init)
-
-    v = np.exp(ln_v)
+    num = 100
+    z = np.linspace(z_init,0, num)
+    
+    #step = (z_init/num )
+    #print(step)
+    
+    print("H: ", H)
+    print("A: ", A)
+    print("rho_0: ", rho_0)
+    print("m: ", m)
+    print("Theta: ", Theta)
+    
+    
+    alpha = (H * A * rho_0)/(2*m*np.sin(Theta))
+    beta = np.exp(-z_init/H)
+    
+    
+    v = v_init * np.exp( alpha*beta - alpha * np.exp(-z/H))
+    
+    
+    
+    
     v_diff = np.diff(v)
+    v_diff = np.append(v_diff, v_diff[-1])
     
     z_diff = np.diff(z)
-    
+    z_diff = np.append(z_diff,z_diff[-1])
     
     _1kT = 4.184E12
     
     KE = (1/2 * m *v**2)
-    KE_kT = KE/_1kT
-    KE_diff = np.diff(KE_kT)/step
-    KE_diff = np.append(KE_diff, 0)
+    KE_diff = np.diff(KE)
+    KE_diff = np.append(KE_diff,KE_diff[-1])
+    
+    KE_unit = abs(KE_diff/z_diff) * 1e3/_1kT
+    print(np.max(KE_unit))
     
     
     
-#    KE_lost_kT
-    
-    plt.plot(KE_diff, z)
-#    plt.xlim(, 0)
+        
+    plt.plot(np.abs(KE_diff/(z_diff/1000)) /_1kT, z/1000)
     plt.ylabel("Height above ground (m)")
-    plt.xlabel("KE (kT km^-1)")
+    plt.xlabel("KE (kT km^1)")
+    plt.show()
     
-    return z_diff
-
-print(plot())
-
-plt.figure()
 plot()
