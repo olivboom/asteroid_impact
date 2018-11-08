e9287aa400a3ef49fcc73c0403c625943070a8c6
MT5918 <43777584+MT5918@users.noreply.github.com>
2018-11-08 10:05:43 +0000

    deleted superfluous tests

--- erostests.py
+++ erostests.py
@@ -1,56 +1,32 @@
 import eroscode.py
 
-def test_speed():
+def test_speed(v):
     # assert that v is decreasing
-    v,m,theta,z,x,KE,r = main()
     assert all(np.around(np.diff(v))<=0)
     
     
-def test_mass1():
-    # assert that mass is not exceeded
-    v,m,theta,z,x,KE,r = main()
-    assert all(m <= m[0])
-    
-    
-def test_mass2():
+def test_mass(m):
     # assert that m is decreasing
-    v,m,theta,z,x,KE,r = main()
     assert all(np.around(np.diff(m)) < 0)
     
     
-def test_alt1():
-    # assert that z is not exceeded
-    v,m,theta,z,x,KE,r = main()
-    assert all(z <= z[0])
-    
-    
-def test_alt2():
+def test_alt(z):
     # assert that altitude is decreasing
-    v,m,theta,z,x,KE,r = main()
     assert all(np.diff(z) <= 0)
     
     
-def test_alt3():
+def test_alt2(r,):
     # assert crater event for large astroids
-    v,m,theta,z,x,KE,r = main()
     if r[0]>100:
-        assert z[-1] <= 0
+        assert burst_index==0
     else:
-        assert all(z > 0)
-    
-def test_dist1():
-    # assert x is not underceeded
-    v,m,theta,z,x,KE,r = main()
-    assert all(x >= 0)
-    
-    
-def test_dist2():
+        assert burst_index==1
+     
+def test_dist(x):
     # assert x is increasing
-    v,m,theta,z,x,KE,r=main()
     assert all(np.diff(x) >= 0)
     
     
-def test_KE():
+def test_KE(KE,z):
     # assert KE is bounded
-    v,m,theta,z,x,KE,r=main()
     assert all(0<=KE/z*1e3 / 4.18E12) and all(KE/z*1e3 / 4.18E12<=10**4)
